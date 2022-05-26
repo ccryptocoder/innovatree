baacede0fb8c8c7bd72279fb6ed721214d7e2183
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-05-26 16:29:08 +0600

    About, students: responsiveness

---
+++ dist/js/header.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menuProjs = document.querySelector('[data-menu-projects]');
+    const menuDropdown =document.querySelector('[data-menu-dropdown]');
+
+    ['mouseover', 'click'].forEach(event => {
+        menuProjs.addEventListener(event, (e) => {
+            e.preventDefault();
+            menuDropdown.classList.add('menu__projects_active')
+        })
+    })
+
+    menuProjs.addEventListener('mouseout', () => {
+        menuDropdown.classList.remove('menu__projects_active')
+    })
+
+    const hamburger = document.querySelector('[data-hamburger]');
+    const menuMobile = document.querySelector('[data-menu-mobile]');
+    const menuItems = Array.from(document.querySelectorAll('[data-menu-items]'));
+
+    hamburger.addEventListener('click', () => {
+        menuMobile.classList.add('header__mobile_active');
+    });
+
+    menuMobile.addEventListener('click', (e) => {
+        e.target.classList.remove('header__mobile_active');
+    })
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            menuMobile.classList.remove('header__mobile_active');
+        })
+    })
+
+});
