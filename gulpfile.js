482f39eded3c49fec80f7091a8795b89b7754618
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-06-06 12:51:18 +0600

    Modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/en/*.html").on('change', gulp.parallel('html-en'));
     gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
     gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
